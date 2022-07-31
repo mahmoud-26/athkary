a1f774f6b0cfe28ba13d07667ba2783956919bbc
mnaggar2006@gmail.com <mnaggar2006@gmail.com>
2022-07-31 23:10:23 +0200

    Adding main files

---
+++ pages/evening-azkar.js
@@ -0,0 +1,34 @@
+const container = document.querySelector(".container");
+
+fetch("../json/azkar.json")
+  .then(res => res.json())
+  .then(result => {
+    for (let i = 0; i < result.evening_azkar.length; i++) {
+      let cardDiv = document.createElement("div");
+      let countDiv = document.createElement("div");
+      let countText = document.createTextNode(result.evening_azkar[i].count);
+      let contentDiv = document.createElement("div");
+      let contentText = document.createTextNode(result.evening_azkar[i].content);
+      cardDiv.setAttribute("class", "card");
+      cardDiv.setAttribute("dir", "rtl");
+      countDiv.setAttribute("class", "count")
+      contentDiv.setAttribute("class", "content");
+      countDiv.appendChild(countText);
+      contentDiv.appendChild(contentText);
+      cardDiv.appendChild(countDiv);
+      cardDiv.appendChild(contentDiv);
+      container.appendChild(cardDiv);
+      cardDiv.addEventListener("click", () => {
+        if (countDiv.innerHTML > 1) {
+          countDiv.innerHTML = parseInt(countDiv.innerHTML) - 1;
+        }
+        else {
+          countDiv.innerHTML = "✓";
+          countDiv.style.color = "gray";
+          countDiv.style.fontSize = "26px";
+          cardDiv.classList.add("card-disabled");
+          contentDiv.classList.add("content-disabled");
+        }
+      });
+    }
+  });
